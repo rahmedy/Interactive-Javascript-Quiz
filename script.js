95ff8fe0af3f0396bc5f76ca4950d0c0a5b34fd9
Rayan AHmedy <rayanahmedy1999@gmail.com>
2020-07-07 03:00:09 -0400

    25% completion

---
+++ script.js
@@ -0,0 +1,25 @@
+let startButton = document.getElementById("main")
+let questionContainer = document.getElementById("question-container")
+
+
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz(){
+console.log("YES")
+startButton.classList.add("hide")
+questionContainer.classList.remove("hide")
+}
+
+function nextQuestion(){
+
+
+
+}
+
+function chooseAnswer(){
+
+
+
+
+
+}
